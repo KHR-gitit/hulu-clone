bdb75a3843a373f2a1ea4a2cecdc25226cb51655
KHR-gitit <khalilrohanidev@gmail.com>
2021-08-09 17:14:52 +1000

    some bug fixed

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,10 +5,10 @@ const Thumbnail = forwardRef(({result}, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
         <div ref={ref} className="group cursor-pointer p-2 duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-            <Image 
+            <Image  
             layout="responsive" src={`
             ${BASE_URL}${result.backdrop_path || result.poster_path}
-            ` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920}
+            ` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} alt=""
             />
             <div>
                 <p className="truncate max-w-md">{result.overview}</p>
